aec41e9f672047d3c12c8cabfc479a9a5e19ad9f
Shubh Neta <shubhnetaorg-a11y@gmail.com>
2026-07-08 10:32:17 +0530

    add sitemap and banner section

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,23 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <section className="banner">
+      <div className="banner-content">
+        <h1>
+          India's Trusted Property Exchange Network
+        </h1>
+
+        <p>
+          Buy, Sell & Invest in Verified Properties with Open Market Bidding
+        </p>
+
+        <button>
+          Explore Properties
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default Banner;
