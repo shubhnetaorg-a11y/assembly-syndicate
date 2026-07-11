bb7a358943bc13e7ac68108176ea85611e042565
Shubh Neta <shubhnetaorg-a11y@gmail.com>
2026-07-11 12:42:04 +0530

    Restore homepage sections

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import SearchBar from "./components/SearchBar";
+import Features from "./components/Features";
+import PropertySection from "./components/PropertySection";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <SearchBar />
+      <Features />
+      <PropertySection />
     </>
   );
 }
