d3da1dccaf83951042277ccc38a31ffa1b9dc29f
Shubh Neta <shubhnetaorg-a11y@gmail.com>
2026-07-11 12:38:23 +0530

    Remove Banner component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Banner from "./components/Banner";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      <Banner />
     </>
   );
 }
